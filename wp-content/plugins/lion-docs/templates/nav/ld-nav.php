04227e5288da600f922bd484eaa60a6c7b834762
Alex Lord <Alexander2.Lord@live.uwe.ac.uk>
2019-06-09 20:45:24 +0100

    upload images doc

--- wp-content/plugins/lion-docs/templates/nav/ld-nav.php
+++ wp-content/plugins/lion-docs/templates/nav/ld-nav.php
@@ -32,6 +32,7 @@
             <span class="level-2">
                 <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/the-text-editor.pdf', 'title' => 'The Text Editor')); ?>
                 <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/password-change.pdf', 'title' => 'Password Change / Password Reset')); ?>
+                <?php echo $nav->render('ld-nav-link', array('filename' => plugins_url() . '/lion-docs/docs/pdf/general/upload-images.pdf', 'title' => 'Upload Images')); ?>
             </span>
         </div>
     </div>
