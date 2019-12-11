b8eed7868934ccffc79d1c9a648dc07d251bba33
admin <alex.lord9@hotmail.co.uk>
2019-12-11 16:44:54 +0000

    docs: whitespace changes

--- wp-content/themes/lioninn/gallery.js
+++ wp-content/themes/lioninn/gallery.js
@@ -1,19 +1,19 @@
 /**
  * Handle Front-End Gallery
  */
-jQuery(function($) {
-    
-    $('.figure').on('click', function() {
+jQuery(function ($) {
+
+    $('.figure').on('click', function () {
         var data = {
             'action': 'load_images_ajax',
             gallery: $(this).parent().data("gallery")
         };
 
         $('.modal-title').text($(this).parent().data("gallery"));
-        
-        $.post(the_ajax_script.ajaxurl, data, function(response) {
+
+        $.post(the_ajax_script.ajaxurl, data, function (response) {
             $('#gallery-images').html(response);
         });
     });
-        
+
 });
