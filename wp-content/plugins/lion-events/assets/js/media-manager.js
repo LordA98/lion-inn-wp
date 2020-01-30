ba72b05186474dedeba1f96e32a2d85c77a17dc8
Alex Lord <Alexander2.Lord@live.uwe.ac.uk>
2020-01-30 16:12:03 +0000

    Reveal everything for the regular events icon select too

--- wp-content/plugins/lion-events/assets/js/media-manager.js
+++ wp-content/plugins/lion-events/assets/js/media-manager.js
@@ -78,17 +78,6 @@ jQuery(function($) {
             $('input[name="edit-event-icon"').val(image_url);
             $('#edit-icon-selected-name').text(image_url);
         });
-
-        // Change to media library tab
-        //$('.media-menu-item').toggleClass('active');
-        // Set filter to only icons - 'Icons' category has a value of '10' for some reason
-        // $("select#media-attachment-filters").val("6").change();
-        // Hide selects so user cannot change filter
-        // $("div.media-toolbar-secondary").hide();
-        // Hide 'Upload Media' & 'Select Media' tabs - we only want selecting media
-        // $('.media-router').hide();
-        // Hide right-idebar (contains selected images details) as it isn't really needed
-        // $('.media-sidebar').hide();
     });
 
 });
